5475f5b9a25c5b9bccb54462286a0977195aad21
hedilict <zn950101@gmail.com>
2020-03-25 07:09:33 +0800

    🎨 update

--- langkhach/Surge/nextdns_linkedip.js
+++ langkhach/Surge/nextdns_linkedip.js
@@ -1,6 +1,6 @@
 //Script event auto linked ipv4 nextdns: network-change
 
-$httpClient.get('https://link-ip.nextdns.io/3994b1/40bcbadb4693c2a5', function(error, response, data){
+$httpClient.post('https://link-ip.nextdns.io/3994b1/40bcbadb4693c2a5', function(error, response, data){
   if (error) {
 console.log(error + '‼️');
   } else {
