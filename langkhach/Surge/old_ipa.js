0ae483f3e656bc62c0bd9d9cbe55ba0c4970c899
hedilict <zn950101@gmail.com>
2020-04-15 07:24:20 +0800

    ✨ Old_iPA_Downloader

---
+++ langkhach/Surge/old_ipa.js
@@ -0,0 +1,22 @@
+/*
+Old_iPA_Downloader by LangKhach
+*/
+var url = $request.url;
+var obj = $request.body;
+
+const api= "unlimapps";
+const buy= "buyProduct";
+
+if(url.indexOf(api) != -1){
+var appidget = url.match(/\d{6,}$/);
+console.log("🟢\n appid: " + appidget);
+$persistentStore.write(appidget.toString(),"appid");
+$notification.post('Old_iPA_Dowloader', 'iTunes PC search app and click Get', 'By @Lãng Khách');
+$done({body});
+}
+if(url.indexOf(buy) != -1){ 
+var appid= $persistentStore.read("appid");
+var body= obj.replace(/\d{6,}/, appid);
+console.log('🟢 Old_iPA_Downloader \nappid: ' + appid);
+$done({body});
+}
