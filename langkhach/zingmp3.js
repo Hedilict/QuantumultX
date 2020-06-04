dd9bd1ad47808e6fa57fc6993792c24df3019b95
hedilict <zn950101@gmail.com>
2020-06-04 18:17:16 +0800

    🚑 【百度贴吧】

--- langkhach/zingmp3.js
+++ langkhach/zingmp3.js
@@ -1,2 +1,18 @@
-body = $response.body.replace(/"lastVipExpireTime":+\d{10}/g, "\"vip\":{\"expireTime\":1892800999,\"startTime\":1572527803,\"subscription\":{\"status\":1,\"expireTime\": 1892800999,\"platform\":2},\"vipType\":1}");
-$done({body});
+
+let obj= JSON.parse($response.body);
+
+//fake user vip
+delete obj.data.lastVipExpireTime;
+obj.data.vip= {
+  "expireTime": 1892800999,
+  "startTime": 1572527803,
+  "subscription": {
+    "status": 0,
+    "expireTime": 1892800999,
+    "platform": 2
+  },
+  "vipType": 1
+}
+
+$done({body:JSON.stringify(obj)});
+
