db2c1487e078291712d35414cbfb4a7a9bb38dce
hedilict <zn950101@gmail.com>
2020-08-10 13:52:40 +0800

    💄 【Netease snail reading unlock vip】

--- NobyDa/QuantumultX/File/wnyd.js
+++ NobyDa/QuantumultX/File/wnyd.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
 Netease snail reading unlock vip
 
@@ -19,23 +19,3 @@ body = JSON.stringify(obj);
 $done({body});
 
 //By yxiaocai & JO2EY
-=======
-/*
-Netease snail reading unlock vip
-By yxiaocai & JO2EY
-QX:
-^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
-
-Surge4：
-http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
-
-Surge & QX MITM = p.du.163.com
-*/
-
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj.tradeEndTime = 1679685290000;
-body = JSON.stringify(obj);
-$done({body});
->>>>>>> b7dc3a0c1aac11f388c88a495f19259999fe4e21
